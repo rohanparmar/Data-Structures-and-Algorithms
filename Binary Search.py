11207aee04c912c0cf63265fede0e007f4e99e47
Rohan Parmar <rohanxparmar@gmail.com>
2022-01-24 15:11:07 -0800

    Create Binary Search.py

---
+++ Binary Search.py
@@ -0,0 +1,32 @@
+# Binary Search eliminates half a list each iteration
+# Very good for searching ordered list of elements
+# Could be good for searching products on eCommerce Websites?
+
+def binary_search(list, item): 
+    
+    low = 0
+    high = len(list) - 1
+    
+    while low <= high: 
+        mid = (low + high) 
+        guess = list[mid] 
+        
+        if guess == item:
+            return mid
+        
+        if guess > item: 
+            high = mid - 1
+
+        else:
+            low = mid + 1
+    return None
+
+my_list = [1, 3, 5, 7, 9]
+
+print()
+print("Starting the program: ")
+print()
+print("Searching for : 3 ")
+print(binary_search(my_list, 3)) # => 1 
+print("Searching for : -1 ")
+print (binary_search(my_list, -1)) # => None
